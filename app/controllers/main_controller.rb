21d46ec0680a078c0537cd373106451f9d572fff
yoel <yoelblumenator@gmail.com>
2015-06-07 19:25:27 +0300

    removed unnecessary crap

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,6 +3,7 @@ class MainController < ApplicationController
     if !(session.has_key? :user_id)
       new_user = User.create!
       session[:user_id] = new_user.id
+      logger.debug "new user entered the app #{new_user.id}"
     end
   end
 end
